e13efb47270b39dea59e73cac904bf8aaeb4c996
krishnapal singh rawat <krishkprawat@gmail.com>
2024-02-26 15:11:08 +0530

    message

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action ="Store", default="chrome"
+    )
 @pytest.fixture(scope="class")
 def setup(request):
-    driver = webdriver.Chrome()
-    driver.get("https://rahulshettyacademy.com/angularpractice/")
+    browser_name=request.config.getOption("browser_name")
+    if browser_name=="chrome":
+
+        driver = webdriver.Chrome()
+        driver.get("https://rahulshettyacademy.com/angularpractice/")
+    elif browser_name=="firefox":
+        #firefox invocation
     request.cls.driver = driver
     yield
     driver.close()
